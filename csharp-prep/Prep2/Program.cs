58fc6025548954f477bd75f6fe8148ed16348588
eldonjmcclure-ops <eldonjmcclure@gmail.com>
2026-04-24 23:46:02 -0600

    Updated Prep2 Program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What is your grade percentage? ");
+        string valueFromUser = Console.ReadLine();
+        int gradePercent = int.Parse(valueFromUser);
+        string letterGrade = "";
+        int signRemainder = gradePercent % 10;
+        string plusMinus = "";
+        
+        if (gradePercent >= 90)
+        {
+            letterGrade = "A";
+        }
+
+        else if (gradePercent >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letterGrade = "C";
+        }
+
+        else if (gradePercent >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        if (signRemainder >= 7 && letterGrade != "A" && letterGrade != "F")
+        {
+            plusMinus = "+";
+        }
+        else if (signRemainder <= 3 && letterGrade != "F")
+        {
+            plusMinus = "-";
+        }
+
+        Console.WriteLine($"Your grade is {letterGrade + plusMinus}!");
+        
+        if (gradePercent > 70)
+        {
+            Console.WriteLine("You Pass!");
+        }
+
+        else
+        {
+            Console.WriteLine("You Didn't Pass.  Better Luck Next Time! ");
+        }
     }
 }
