0a87c5c5ea879618359991af1c5e1d25c9d39f3b
eldonjmcclure-ops <eldonjmcclure@gmail.com>
2026-04-27 14:01:15 -0600

    Update Program.cs

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        // bool yes = false;
+        for (int i = 1; i < 10; i += 2)
+        {
+            Console.WriteLine($"Number {i}");
+        }
+
     }
 }
