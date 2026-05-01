aa924625454e4bdc4ddf6aa629a0da4c8cf3dd49
eldonjmcclure-ops <eldonjmcclure@gmail.com>
2026-04-30 23:56:27 -0600

    added Prove 3 4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+
+        // Ask for and record a guess
+        
+        // Compare the guess and the magic number
+        string loopActive = "true";
+        while (loopActive == "true")
+        {
+            Console.Write("What is your guess? ");
+            string userValue = Console.ReadLine();
+            int userGuess = int.Parse(userValue);
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it right!");
+                loopActive = "false";
+            }
+        }
+
+        // Give an answer corresponding to the closeness of the guess
+
     }
-}
+} 
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Globalization;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Compute the sum, or total, of the numbers in the list.
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        string userValue;
+        List<int> numbers = new();
+        do
+        {
+            Console.Write("Enter Number: ");
+            userValue = Console.ReadLine();
+            int number = int.Parse(userValue);
+            numbers.Add(number);
+            
+        } while (userValue != "0");
+        int sum = 
+        Console.Write("The sum is: ");
+        // Compute the average of the numbers in the list.
+
+        // Find the maximum, or largest, number in the list.
+
     }
 }
