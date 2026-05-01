8fe9ce7914ac3af1933fc2c4148ec790246a346f
eldonjmcclure-ops <eldonjmcclure@gmail.com>
2026-05-01 11:12:01 -0600

    Update Program.cs

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -18,9 +18,15 @@ class Program
             numbers.Add(number);
             
         } while (userValue != "0");
-        int sum = 
-        Console.Write("The sum is: ");
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
         // Compute the average of the numbers in the list.
+        double average = numbers.Average();
+        Console.WriteLine($"The average is: {average}");
+
+        int largest = numbers.Max();
+        Console.WriteLine($"The largest number is: {largest}");
 
         // Find the maximum, or largest, number in the list.
 
