ba82f59c1a2a74742d9b41329a45fee2a03e5304
eldonjmcclure-ops <eldonjmcclure@gmail.com>
2026-05-04 01:06:06 -0600

    Did Resume Assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job newJob = new Job();
+        newJob._jobTitle = "Head Engineer";
+        newJob._company = "Endfield Industries";
+        newJob._startYear = 2040;
+        newJob._endYear = 2498;
+
+        Job newJob2 = new Job();
+        newJob2._jobTitle = "Evil Scientist";
+        newJob2._company = "Doofenschmirtz Evil Inc.";
+        newJob2._startYear = 2020;
+        newJob2._endYear = 2034;
+
+        Resume newResume = new Resume();
+        newResume._name = "Allison";
+        newResume._jobList.Add(newJob);
+        newResume._jobList.Add(newJob2);
+        
+        newResume.DisplayList();
     }
 }
