852a30c7466a9027338e8aed4aabfd406b2b6eab
eldonjmcclure-ops <eldonjmcclure@gmail.com>
2026-06-01 03:22:54 -0600

    Added Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Indiana Jones", "Archaeology");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Jim McCool", "Calculus", "2.6", "7-18");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Tony Stark", "History of Robotics", "The Pathway to Modern-Day Robots");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
